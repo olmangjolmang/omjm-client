d0d67f8d639ff2a0b43b2a5f3c422767fb91fed8
jisooooooooooo <dlawltn0708@naver.com>
2024-07-01 12:17:34 +0900

    ✅ Test: 브랜치 생성 및 병합 테스트

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Article from "./Article";
 
 const Main: React.FC = () => {
   return (
     <div>
       <Header />
-      <Article />
+      
       <Footer />
     </div>
   );
