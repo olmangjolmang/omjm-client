e88e6d020cd600a31fa30449d3dad5c180926e4c
jisooooooooooo <dlawltn0708@naver.com>
2024-07-11 20:41:59 +0900

    ✨ Feat: 로그인 에러메시지 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent, FormEvent } from "react";
+import React, { useState, ChangeEvent, FormEvent, FocusEvent } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
@@ -21,14 +21,14 @@ const Label = styled.label`
   margin-bottom: 15px;
 `;
 
-const Input = styled.input`
+const Input = styled.input<{ hasError: boolean }>`
   width: 515px;
   height: 60px;
   border-radius: 10px;
   font-size: 16px;
   color: #afafb6;
-  border: solid 1px;
-  margin-bottom: 20px;
+  border: solid 1px ${(props) => (props.hasError ? "red" : "#dfdfe5")};
+  margin-bottom: 5px;
   padding-left: 19px;
 `;
 
@@ -45,6 +45,12 @@ const Button = styled.button<{ disabled: boolean }>`
   cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
 `;
 
+const ErrorMessage = styled.div`
+  color: #f44;
+  font-size: 14px;
+  margin-bottom: 20px;
+`;
+
 const BottomText = styled.div`
   font-size: 16px;
   margin-top: 20px;
@@ -59,20 +65,63 @@ const SignupText = styled(Link)`
   margin-top: 12px;
 `;
 
+interface Errors {
+  email: string;
+  password: string;
+}
+
+interface Touched {
+  email: boolean;
+  password: boolean;
+}
+
 export const Login: React.FC = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(true);
+  const [errors, setErrors] = useState<Errors>({
+    email: "",
+    password: "",
+  });
+  const [touched, setTouched] = useState<Touched>({
+    email: false,
+    password: false,
+  });
 
   React.useEffect(() => {
-    setIsButtonDisabled(!(email && password));
+    const emailError = validateEmail();
+    const passwordError = validatePassword();
+
+    setErrors({
+      email: emailError,
+      password: passwordError,
+    });
+
+    setIsButtonDisabled(!!emailError || !!passwordError);
   }, [email, password]);
+
+  const validateEmail = (): string => {
+    if (!email) {
+      return "이메일을 입력해 주세요.";
+    }
+    return "";
+  };
+
+  const validatePassword = (): string => {
+    if (!password) {
+      return "비밀번호를 입력해 주세요.";
+    }
+    return "";
+  };
+
+  const handleBlur = (field: keyof Touched) => () => {
+    setTouched((prev) => ({ ...prev, [field]: true }));
+  };
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (!email || !password) {
-    } else {
+    if (!errors.email && !errors.password) {
       // 로그인 로직 추가
       console.log("로그인 성공");
     }
@@ -90,7 +139,12 @@ export const Login: React.FC = () => {
             onChange={(e: ChangeEvent<HTMLInputElement>) =>
               setEmail(e.target.value)
             }
+            onBlur={handleBlur("email")}
+            hasError={touched.email && !!errors.email}
           />
+          {touched.email && errors.email && (
+            <ErrorMessage>{errors.email}</ErrorMessage>
+          )}
         </Box>
         <Box>
           <Label>비밀번호</Label>
@@ -101,7 +155,12 @@ export const Login: React.FC = () => {
             onChange={(e: ChangeEvent<HTMLInputElement>) =>
               setPassword(e.target.value)
             }
+            onBlur={handleBlur("password")}
+            hasError={touched.password && !!errors.password}
           />
+          {touched.password && errors.password && (
+            <ErrorMessage>{errors.password}</ErrorMessage>
+          )}
         </Box>
 
         <Button type="submit" disabled={isButtonDisabled}>
