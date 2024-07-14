0f4748bbef2f317cebb4c30b468520de269d16a0
jisooooooooooo <dlawltn0708@naver.com>
2024-07-14 21:28:17 +0900

    ✏️ Chore: 스타일 분리

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,76 +1,15 @@
-import React, {
-  useState,
-  useEffect,
-  ChangeEvent,
-  FormEvent,
-  FocusEvent,
-} from "react";
-import styled from "styled-components";
+import React, { useState, useEffect, ChangeEvent, FormEvent } from "react";
 import { Link } from "react-router-dom";
-
-const Title = styled.div`
-  font-size: 24px;
-  display: flex;
-  flex-direction: row;
-  font-weight: 600;
-  margin-bottom: 45px;
-`;
-
-const Ticle = styled.div`
-  color: #463efb;
-  font-size: 24px;
-  font-weight: 600;
-`;
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-`;
-
-const Box = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 20px;
-`;
-
-const Label = styled.label`
-  font-size: 16px;
-  font-weight: 500;
-  margin-bottom: 15px;
-`;
-
-const Input = styled.input<{ hasError: boolean }>`
-  width: 515px;
-  height: 60px;
-  border-radius: 10px;
-  font-size: 16px;
-  color: #afafb6;
-  border: solid 1px ${(props) => (props.hasError ? "red" : "#dfdfe5")};
-  margin-bottom: 5px;
-  padding-left: 19px;
-`;
-
-const Button = styled.button<{ disabled: boolean }>`
-  width: 534px;
-  height: 65px;
-  background-color: ${(props) => (props.disabled ? "#F4F4F7" : "#e5efff")};
-  color: ${(props) => (props.disabled ? "#AFAFB6" : "#463efb")};
-  border: none;
-  border-radius: 20px;
-  font-size: 20px;
-  font-weight: 600;
-  margin-top: 8px;
-  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
-`;
-
-const ErrorMessage = styled.div`
-  color: #f44;
-  font-size: 14px;
-  margin-bottom: 20px;
-`;
+import {
+  Title,
+  Ticle,
+  Container,
+  Box,
+  Label,
+  Input,
+  Button,
+  ErrorMessage,
+} from "../styles/Signup";
 
 interface Errors {
   email: string;
