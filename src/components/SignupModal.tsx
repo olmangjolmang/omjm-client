d2dc396fdbfd55e5da20beb77134ae0d3b49273b
jisooooooooooo <dlawltn0708@naver.com>
2024-07-16 17:15:53 +0900

    ✏️ Chore 스타일 분리

--- src/components/SignupModal.tsx
+++ src/components/SignupModal.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
 import img from "../assets/signupmodalimg.png";
+import {
+  ModalOverlay,
+  ModalContent,
+  CloseButton,
+  Message,
+  Img,
+  SubMessage,
+  HighlightText,
+  ProfileButton,
+} from "../styles/SignupModal";
 
 interface ModalProps {
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -59,75 +68,3 @@ const SignupModal: React.FC<ModalProps> = ({ setIsModalOpen }) => {
 };
 
 export default SignupModal;
-
-const ModalOverlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1000;
-`;
-
-const ModalContent = styled.div`
-  background: white;
-  padding: 88px 132px;
-  border-radius: 8px;
-  position: relative;
-  text-align: center;
-`;
-
-const CloseButton = styled.button`
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  background: none;
-  border: none;
-  cursor: pointer;
-`;
-
-const Message = styled.h2`
-  font-weight: 700;
-  font-size: 24px;
-  line-height: 150%;
-  letter-spacing: -0.12px;
-`;
-
-const Img = styled.img`
-  margin: 38px 0;
-  width: 192px;
-  height: 160px;
-`;
-
-const SubMessage = styled.p`
-  margin-bottom: 2rem;
-  font-size: 20px;
-  font-weight: 600;
-  line-height: 150%;
-  letter-spacing: -0.1px;
-`;
-
-const HighlightText = styled.span`
-  color: #463efb;
-  font-weight: 600;
-  font-size: 20px;
-  line-height: 150%;
-  letter-spacing: -0.1px;
-`;
-
-const ProfileButton = styled.button`
-  background: #e5efff;
-  color: #463efb;
-  border: none;
-  width: 515px;
-  height: 65px;
-  padding: 10px;
-  font-weight: 600;
-  font-size: 20px;
-  border-radius: 20px;
-  cursor: pointer;
-`;
