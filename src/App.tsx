398b3da9efb31a5ddf9a16b4d66a2edc0e6944d8
Sungyeon Jung <yeonnybus@gmail.com>
2024-07-19 18:41:18 +0900

    ✨ Feat: axios default URL setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import axios from "axios";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import MainPage from "./pages/MainPage";
 import Main from "./pages/Main";
 import { Login } from "./pages/Login";
@@ -8,20 +10,27 @@ import { Signup } from "./pages/Signup";
 import { SignupAgree } from "./pages/SignupAgree";
 import ProfileEdit from "./pages/ProfileEdit";
 import InterestEdit from "./pages/InterestEdit";
+import ArticleBoards from "./pages/ArticleBoards";
+
+axios.defaults.baseURL = "http://3.36.247.28";
+const queryClient = new QueryClient();
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/article" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signupagree" element={<SignupAgree />} />
-        <Route path="/profile-edit" element={<ProfileEdit />} />
-        <Route path="/edit-interests" element={<InterestEdit />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/article" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signupagree" element={<SignupAgree />} />
+          <Route path="/profile-edit" element={<ProfileEdit />} />
+          <Route path="/edit-interests" element={<InterestEdit />} />
+          <Route path="articles" element={<ArticleBoards />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 };
 
