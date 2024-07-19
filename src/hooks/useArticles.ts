d8ad81d4f247bcb86c72de236d597d9202b458b3
Sungyeon Jung <yeonnybus@gmail.com>
2024-07-19 18:42:10 +0900

    ✨ Feat: 아티클 목록 불러오기 useQuery Hook

---
+++ src/hooks/useArticles.ts
@@ -0,0 +1,33 @@
+import { useQuery, QueryKey, UseQueryOptions } from "@tanstack/react-query";
+import axios from "axios";
+import { Category, OrderBy } from "../types/ArticleBoards";
+
+const fetchArticles = async (
+  page?: number,
+  category?: Category,
+  orderBy?: OrderBy
+) => {
+  const { data } = await axios.get(`/post`, {
+    params: {
+      category,
+      orderBy,
+      page,
+    },
+  });
+  return data;
+};
+
+const useArticles = (
+  page?: number,
+  category?: Category,
+  orderBy?: OrderBy,
+  options?: UseQueryOptions
+) => {
+  return useQuery({
+    queryKey: ["articles", page, category, orderBy],
+    queryFn: () => fetchArticles(page, category, orderBy),
+    placeholderData: (previousData, previousQuery) => previousData,
+  });
+};
+
+export { useArticles };
