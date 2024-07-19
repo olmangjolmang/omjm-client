08c06525720c59debd1a8e2405f0cda78004d4fe
Sungyeon Jung <yeonnybus@gmail.com>
2024-07-19 19:23:34 +0900

    ✨ Feat: 아티클 목록 페이지 구성

--- src/pages/ArticleBoards.tsx
+++ src/pages/ArticleBoards.tsx
@@ -1,19 +1,28 @@
 import React from "react";
-import PostBoard from "../components/Boards";
+import Board from "../components/Boards";
 import styled from "styled-components";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 20px;
+  min-height: 100vh;
+  max-width: 1440px;
+  min-width: 1000px;
 `;
 
-const ArticleBoards: React.FC = () => {
+const ArticleBoards = () => {
   return (
-    <PageContainer>
-      <PostBoard />
-    </PageContainer>
+    <>
+      <Header />
+      <PageContainer>
+        <Board />
+      </PageContainer>
+      <Footer />
+    </>
   );
 };
 
