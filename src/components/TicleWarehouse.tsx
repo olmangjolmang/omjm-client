4dcaf7be7d8f38e1915b54b1564fc6aed79594f1
Sungyeon Jung <yeonnybus@gmail.com>
2024-07-22 02:57:14 +0900

    ✨ Feat: 마이페이지 티클창고 dummy 컴포넌트

---
+++ src/components/TicleWarehouse.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useArticles } from "../hooks/useArticles";
+import PostItemMain from "./PostItemMain";
+import CategorySelector from "./CategorySelector";
+import Pagination from "./Pagination";
+import Select from "./Select";
+import { Category, OrderBy, CATEGORY_LABELS } from "../types/ArticleBoards";
+import { Container, GridContainer } from "../styles/ArticleBoards";
+import { Option } from "../types/ArticleBoards";
+
+const orderByOptions: Option[] = [
+  { value: "LATEST", label: "최신순" },
+  { value: "OLDEST", label: "오래된순" },
+  { value: "SCRAPPED", label: "스크랩순" },
+];
+
+const Boards = () => {
+  const [page, setPage] = useState(1);
+  const [category, setCategory] = useState<Category | undefined>(undefined);
+  const [orderBy, setOrderBy] = useState<OrderBy>("LATEST");
+
+  const { data, isLoading, isError } = useArticles(page, category, orderBy);
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // 페이지가 변경될 때마다 화면을 상단으로 스크롤합니다.
+  }, [page]);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error loading articles</div>;
+  console.log(data);
+  return (
+    <Container>
+      <div>
+        <Select
+          value={orderBy}
+          onChange={(e) => setOrderBy(e.target.value as OrderBy)}
+          options={orderByOptions}
+        />
+      </div>
+      <GridContainer>
+        {data &&
+          data.results.content.map((article: any) => (
+            <PostItemMain
+              key={article.postId}
+              {...article}
+              postCategory={CATEGORY_LABELS[article.postCategory as Category]}
+            />
+          ))}
+      </GridContainer>
+      <Pagination
+        currentPage={page}
+        totalPages={data.results.totalPages}
+        onPageChange={setPage}
+      />
+    </Container>
+  );
+};
+
+export default Boards;
