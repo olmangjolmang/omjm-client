74463bdefb6a3a7f92afc4652569e528b60f50d8
Sungyeon Jung <yeonnybus@gmail.com>
2024-07-22 16:24:22 +0900

    ✨ Feat: 티클창고 불러오기 api 훅 추가

---
+++ src/hooks/useTicleWarehouse.ts
@@ -0,0 +1,33 @@
+import { useQuery, UseQueryOptions } from "@tanstack/react-query";
+import { Category, OrderBy } from "../types/ArticleBoards";
+import axiosInstance from "../api/AxiosInstance";
+
+const fetchWarehouse = async (
+  page?: number,
+  category?: Category,
+  orderBy?: OrderBy
+) => {
+  const { data } = await axiosInstance.get(`/mypage/saved-ticles`, {
+    params: {
+      category: category !== undefined ? category : undefined,
+      page,
+    },
+  });
+  console.log("data는");
+  console.log(data);
+  return data;
+};
+
+const useTicleWarehouse = (
+  page?: number,
+  category?: Category,
+  options?: UseQueryOptions
+) => {
+  return useQuery({
+    queryKey: ["articles", page, category],
+    queryFn: () => fetchWarehouse(page, category),
+    placeholderData: (previousData, previousQuery) => previousData,
+  });
+};
+
+export { useTicleWarehouse };
