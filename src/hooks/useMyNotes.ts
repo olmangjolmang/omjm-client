0da3d0b4ec67abff3bf7163690e6ea27ce3b2f00
Sungyeon Jung <yeonnybus@gmail.com>
2024-07-23 02:52:01 +0900

    🐛 Fix: api 타입 수정 및 리턴 수정

--- src/hooks/useMyNotes.ts
+++ src/hooks/useMyNotes.ts
@@ -2,23 +2,31 @@ import { useQuery, UseQueryOptions } from "@tanstack/react-query";
 import axiosInstance from "../api/AxiosInstance";
 
 // Note 타입 정의
-export interface Note {
+interface Note {
   noteId: number;
   content: string;
   memoDate: number[];
   postId: number;
   postTitle: string;
+  targetText: string;
+}
+
+interface ApiResponse {
+  isSuccess: boolean;
+  code: string;
+  message: string;
+  results: Note[];
 }
 
 // API 호출 함수 정의
-const fetchMyNotes = async (): Promise<Note[]> => {
+const fetchMyNotes = async (): Promise<ApiResponse> => {
   const { data } = await axiosInstance.get("/mypage/my-note");
-  return data.results;
+  return data;
 };
 
 // useMyNotes 훅 정의
-const useMyNotes = (options?: UseQueryOptions<Note[], Error>) => {
-  return useQuery<Note[], Error>({
+const useMyNotes = (options?: UseQueryOptions<ApiResponse, Error>) => {
+  return useQuery<ApiResponse, Error>({
     queryKey: ["myNotes"],
     queryFn: fetchMyNotes,
     ...options,
