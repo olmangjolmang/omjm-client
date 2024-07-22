bb917d640ae540f83ced83f7ea0ce447db91e599
Sungyeon Jung <yeonnybus@gmail.com>
2024-07-23 00:53:29 +0900

    ✨ Feat: 마이페이지 api 및 기본 ui 구현

---
+++ src/components/TicleQnaGroup.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useArticles } from "../hooks/useArticles";
+import PostItemMain from "./PostItemMain";
+import CategorySelector from "./CategorySelector";
+import Pagination from "./Pagination";
+import Select from "./Select";
+import { Category, OrderBy, CATEGORY_LABELS } from "../types/ArticleBoards";
+import { Container, GridContainer } from "../styles/ArticleBoards";
+import { Option } from "../types/ArticleBoards";
+import { useTicleWarehouse } from "../hooks/useTicleWarehouse";
+
+const orderByOptions: Option[] = [
+  { value: "LATEST", label: "최신순" },
+  { value: "OLDEST", label: "오래된순" },
+  { value: "SCRAPPED", label: "스크랩순" },
+];
+
+const TicleQnaGroup = () => {
+  const [page, setPage] = useState(0);
+
+  const { data, isLoading, isError } = useTicleWarehouse(page);
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // 페이지가 변경될 때마다 화면을 상단으로 스크롤합니다.
+  }, [page]);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error loading articles</div>;
+  console.log(data);
+  return (
+    <Container>
+      <GridContainer>
+        {data &&
+          data.results.map((article: any) => (
+            <PostItemMain
+              key={article.postId}
+              {...article}
+              postCategory={CATEGORY_LABELS[article.postCategory as Category]}
+              isSaved={true}
+              isSavedIconLoad={true}
+            />
+          ))}
+      </GridContainer>
+      <Pagination
+        currentPage={page}
+        totalPages={data.results.totalPages}
+        onPageChange={setPage}
+      />
+    </Container>
+  );
+};
+
+export default TicleQnaGroup;
--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -3,12 +3,12 @@ import styled from "styled-components";
 import Header from "./Header";
 import Footer from "./Footer";
 import TicleWarehouse from "../components/TicleWarehouse";
-
-const TicleQnA = () => <div>티클 문답 내용</div>;
-const TicleNotes = () => <div>티클 노트 내용</div>;
+import TicleQna from "../components/TicleQna";
+import TicleNote from "../components/TicleNote";
 
 const MyPage = () => {
   const [activeTab, setActiveTab] = useState<string>("ticleWarehouse");
+  console.log(localStorage.getItem("token"));
   return (
     <>
       <Header />
@@ -35,8 +35,8 @@ const MyPage = () => {
         </TabContainer>
         <TabContent>
           {activeTab === "ticleWarehouse" && <TicleWarehouse />}
-          {activeTab === "ticleQnA" && <TicleQnA />}
-          {activeTab === "ticleNotes" && <TicleNotes />}
+          {activeTab === "ticleQnA" && <TicleQna />}
+          {activeTab === "ticleNotes" && <TicleNote />}
         </TabContent>
       </PageContainer>
       <Footer />
