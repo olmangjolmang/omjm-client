8c410e278068e826d80885f487ab13c05f178cfb
Sungyeon Jung <yeonnybus@gmail.com>
2024-07-23 22:30:49 +0900

    ✨ Feat: 검색 및 네비게이션 기능 구현

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import logo from "../assets/logo.png";
 import axiosInstance from "../api/AxiosInstance";
 import {
@@ -25,8 +26,11 @@ import {
 import profileImg from "../assets/headerprofile.png";
 
 const Header: React.FC = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
@@ -36,10 +40,27 @@ const Header: React.FC = () => {
     try {
       await axiosInstance.delete("/users/logout");
       setIsLoggedIn(false);
-      setIsLoggedIn(false);
       console.log("로그아웃 성공");
     } catch (error) {
       console.error("로그아웃 중 오류 발생:", error);
+    }
+  };
+
+  const handleSearch = () => {
+    if (searchTerm.trim() !== "") {
+      navigate("/articles", { state: { searchTerm } });
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleMenuClick = (path: string) => {
+    if (location.pathname === path) {
+      navigate(path, { replace: true, state: { reload: true } });
+    } else {
+      navigate(path);
     }
   };
 
@@ -48,19 +69,24 @@ const Header: React.FC = () => {
       <LeftContainer>
         <Logo src={logo} alt="로고" />
         <MenuContainer>
-          <Menu to="/">홈</Menu>
-          <Menu to="/articles">아티클</Menu>
-          <Menu to="/">물어봥</Menu>
-          <Menu to="/">마이페이지</Menu>
+          <Menu onClick={() => handleMenuClick("/")}>홈</Menu>
+          <Menu onClick={() => handleMenuClick("/articles")}>아티클</Menu>
+          <Menu onClick={() => handleMenuClick("/ticleQna")}>티클 문답</Menu>
+          <Menu onClick={() => handleMenuClick("/mypage")}>마이페이지</Menu>
         </MenuContainer>
       </LeftContainer>
       <RightContainer>
         <SearchContainer>
-          <SearchInput placeholder="궁금한 내용을 검색해 보세요!" />
+          <SearchInput
+            placeholder="궁금한 내용을 검색해 보세요!"
+            value={searchTerm}
+            onChange={handleInputChange}
+          />
           <SearchIcon
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 23"
             fill="none"
+            onClick={handleSearch}
           >
             <path
               d="M9.49985 16.4999C13.6419 16.4999 16.9997 13.1422 16.9997 9.0001C16.9997 4.85804 13.6419 1.50024 9.49985 1.50024C5.3578 1.50024 2 4.85804 2 9.0001C2 13.1422 5.3578 16.4999 9.49985 16.4999Z"
