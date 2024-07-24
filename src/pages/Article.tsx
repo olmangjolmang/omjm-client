ac7bad197cba6d57fd7f3a511d484641a9be9947
jisooooooooooo <dlawltn0708@naver.com>
2024-07-24 20:59:07 +0900

    💄 Design: 아티클 디자인 수정

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import Header from './Header';
-import Footer from './Footer';
-import axiosInstance from '../api/AxiosInstance';
-import linkimg from '../assets/linkicon.png';
-import articleImg from '../assets/article.png';
-import QuizSection from '../components/QuizSection';
-import FloatingButtons from '../components/FloatingButtons';
-import HighlightModal from '../components/HighlightModal';
-import { useArticle } from '../hooks/useArticle';
+import React from "react";
+import { useParams } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import axiosInstance from "../api/AxiosInstance";
+import linkimg from "../assets/linkicon.png";
+import articleImg from "../assets/article.png";
+import QuizSection from "../components/QuizSection";
+import FloatingButtons from "../components/FloatingButtons";
+import HighlightModal from "../components/HighlightModal";
+import { useArticle } from "../hooks/useArticle";
 import {
   Container,
   Category,
@@ -23,13 +23,14 @@ import {
   Highlight,
   Line,
   BottomArticleTitle,
+  GoodArticleSection,
   GoodArticleContainer,
   GoodArticleImg,
   GoodArticleCategory,
   GoodArticleTitle,
   GoodArticleAuthor,
   Overlay,
-} from '../styles/Article';
+} from "../styles/Article";
 
 const Article: React.FC = () => {
   const { id } = useParams<{ id: string }>();
@@ -37,9 +38,12 @@ const Article: React.FC = () => {
     enabled: !!id,
   });
 
-  const [isHighlightModalOpen, setIsHighlightModalOpen] = React.useState<boolean>(false);
+  const [isHighlightModalOpen, setIsHighlightModalOpen] =
+    React.useState<boolean>(false);
   const [highlightedText, setHighlightedText] = React.useState<string>("");
-  const [highlightedRanges, setHighlightedRanges] = React.useState<Array<{ start: number; end: number }>>([]);
+  const [highlightedRanges, setHighlightedRanges] = React.useState<
+    Array<{ start: number; end: number }>
+  >([]);
 
   const handleTextHighlight = (e: React.MouseEvent) => {
     const selection = window.getSelection();
@@ -51,7 +55,7 @@ const Article: React.FC = () => {
 
       if (text) {
         setHighlightedText(text);
-        setHighlightedRanges(prevRanges => [...prevRanges, { start, end }]);
+        setHighlightedRanges((prevRanges) => [...prevRanges, { start, end }]);
         selection.removeAllRanges(); // Remove selection after capturing it
       }
     }
@@ -146,7 +150,7 @@ const Article: React.FC = () => {
   } = article;
 
   const date = new Date(createdDate);
-  const formattedDate = date.toISOString().split('T')[0];
+  const formattedDate = date.toISOString().split("T")[0];
 
   return (
     <>
@@ -166,7 +170,7 @@ const Article: React.FC = () => {
         </Content>
         <Line />
         <QuizSection title={title} id={Number(id)} />
-        <div>
+        <GoodArticleSection>
           <BottomArticleTitle>함께 읽으면 좋은 아티클</BottomArticleTitle>
           <GoodArticleContainer>
             {recommendPost.length > 0 ? (
@@ -187,7 +191,7 @@ const Article: React.FC = () => {
               <div>추천 아티클이 없습니다.</div>
             )}
           </GoodArticleContainer>
-        </div>
+        </GoodArticleSection>
         {isHighlightModalOpen && (
           <>
             <Overlay isModalOpen={isHighlightModalOpen}>
